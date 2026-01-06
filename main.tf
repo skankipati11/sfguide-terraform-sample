6e16bb7f9924a86d61ef81f2dae7af97c8e2bf59
skankipati11 <surya.kankipati11@gmail.com>
2026-01-06 20:39:06 +0530

    Add Config, Database and Warehouse

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source = "snowflakedb/snowflake"
+    }
+  }
+}
+
+locals {
+  organization_name = "xodjopc"
+  account_name      = "jtc04659"
+  private_key_path  = "~/.ssh/snowflake_tf_snow_key.p8"
+}
+
+provider "snowflake" {
+    organization_name = local.organization_name
+    account_name      = local.account_name
+    user              = "TERRAFORM_SVC"
+    role              = "SYSADMIN"
+    authenticator     = "SNOWFLAKE_JWT"
+    private_key       = file(local.private_key_path)
+}
+
+resource "snowflake_database" "tf_db" {
+  name         = "TF_DEMO_DB"
+  is_transient = false
+}
+
+resource "snowflake_warehouse" "tf_warehouse" {
+  name                      = "TF_DEMO_WH"
+  warehouse_type            = "STANDARD"
+  warehouse_size            = "XSMALL"
+  max_cluster_count         = 1
+  min_cluster_count         = 1
+  auto_suspend              = 60
+  auto_resume               = true
+  enable_query_acceleration = false
+  initially_suspended       = true
+}
